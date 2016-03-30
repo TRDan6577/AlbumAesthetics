1fdff5647f83e6f3e0b6423086b9b75c87bfad7f
trd6577 <trd6577@g.rit.edu>
2016-03-29 23:41:08 -0400

    Added more notes to the main file

--- albumAesthetics.py
+++ albumAesthetics.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import mechanize
+import re
 
 br = mechanize.Browser()
 br.set_handle_robots(False) # Be mean and ignore the site's request to not use robots
@@ -15,3 +16,10 @@ soup = BeautifulSoup(html.read(), 'html.parser')
 # 1) Search result separated by spaces
 # 2) Turn off safe search for all results
 # 3) Set the type of search to an image search
+
+# Then in print(soup.prettify()), look for the a href. Get rid of the 
+# '/url?q=' and replace it with 'https://www.google.com/imgres?imgurl='
+# this will take you to another page. Grab the source html again and put it
+# in the soup to read again.
+# look for '<a target="_blank"...' and grab the url from after 'data-href='
+# This bring you to the image. Download it
