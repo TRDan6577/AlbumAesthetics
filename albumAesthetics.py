ce838e0d05447e42da42bf9d6d4736fe3fdc4ac6
trd6577 <trd6577@g.rit.edu>
2016-04-03 23:51:38 -0400

    Returns the url of the image
    Also added another bullet point under future goals in the readme

--- albumAesthetics.py
+++ albumAesthetics.py
@@ -8,17 +8,23 @@ import re
 import string
 
 MULTIPLICATION = u"\xd7".encode('utf-8') # a multiplication symbol
-TOLERANCE = 20 # max distance from a square image that we'll accept
-IMAGE_SEARCH = "https://www.google.com/imgres?imgurl="
-DELETION_SIZE = 7 # the size of the string "/url?q="
+TOLERANCE = 20  # max distance from a square image that we'll accept
+IMAGE_SEARCH = 15  # Length of "/imgres?imgurl="
+DELETION_SIZE = 7  # the size of the string "/url?q="
+ACCEPTABLE_IMAGE_FORMATS = {".jpg", ".png", ".gif"}
 
+print("[*] Creating the browser")
 br = mechanize.Browser()
+print("[*] Creating the http GET request")
 br.set_handle_robots(False) # Be mean and ignore the site's request to not use robots
 br.addheaders = [('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.125 Safari/537.36')] # pretend we're a browser
-html = br.open("https://www.google.com/search?q=this+is+a+test&safe=off&tbm=isch")
+html = br.open("https://www.google.com/search?q=acdc+back+in+black&safe=off&tbm=isch")
+print("[+] Request sent!")
 
+print("[*] Preparing the delicious soup")
 # prepare some delicious soup
 soup = BeautifulSoup(html.read(), 'html.parser')
+print("[+] Soup ready!")
 
 # URLs for images searches on google:
 # https://www.google.com/search?q=my+search+terms+here&safe=off&tbm=isch
@@ -27,53 +33,48 @@ soup = BeautifulSoup(html.read(), 'html.parser')
 # 2) Turn off safe search for all results
 # 3) Set the type of search to an image search
 
-# Then in print(soup.prettify()), look for the a href.
+# Then in print(soup.prettify()), look for the a href
 
-"""
-come up with a faster way. If you get the urls this way,
-you still need to parse them once more. If you get the URLs
-and their corresponding picture sizes at the same time, you
-only need to go through all of the urls once
-"""
-
+print("[*] Getting the image URLs")
 urls = []
-
 for link in soup.find_all('a'):
     if(link.get('href') is not None and 
        string.find(str(link.get('href')), "/imgres?imgurl=") != -1):
         urls.append(link.get('href'))
+print("[*] Retrieved image URLs")
 
 # To find the size of the image, look for the multiplication sign
 # (unicode) U+00D7 or \xd7
 
 # Get all the sizes
 sizes = soup.find_all(attrs={"class": "rg_ilmn"})
-urlIndex = 0 # Keep track of which URL we're looking at
 
 # Keep track of the highest resolution and it's corresponding index
 highestResolution = 0
 bestIndex = -1
 
 # While we see a multiplication sign in the string, that means we have
-# an image size
-while(string.find(brString, MULTIPLICATION) != -1):
+# an image size. The format of the size string is 
+# " length x width - sourceDomain "
+for i in range(0, 19):
 
-    curIndex = 0
+    curIndex = 1  # There's a space before the length
+    sizeString = sizes[i].getText()
 
     # Set the length
     strLength = ''
-    while(brString[curIndex].isdigit()):
-        strLength = strLength + brString[curIndex]
+    while(sizeString[curIndex].isdigit()):
+        strLength = strLength + sizeString[curIndex]
         curIndex+=1
         # TODO: raise error here if you go out of index
 
     length = int(strLength)
-    curIndex = curIndex + 4 # This puts us at the start of the width
+    curIndex = curIndex + 3  # This puts us at the start of the width
 
     # Set the width
     strWidth = ''
-    while(brString[curIndex].isdigit()):
-        strWidth = strWidth + brString[curIndex]
+    while(sizeString[curIndex].isdigit()):
+        strWidth = strWidth + sizeString[curIndex]
         curIndex+=1
         # TODO: raise error here if you go out of index
     
@@ -83,33 +84,17 @@ while(string.find(brString, MULTIPLICATION) != -1):
     if(abs(length - width) <= TOLERANCE):
         if(length * width > highestResolution):
             highestResolution = length * width
-            bestIndex = urlIndex
+            bestIndex = i
 
-    print("URL = " + urls[urlIndex])
+    print("URL = " + urls[i])
     print("Has an image size of " + strLength + " x " + strWidth)
     print('\n')
 
-    sizeFinder = sizeFinder.find_next('br')
-    if(sizeFinder is None):
-        break
-    sizeFinder = sizeFinder.find_next('br')
-    if(sizeFinder is None):
-        break
-    sizeFinder = sizeFinder.find_next('br')
-    if(sizeFinder is None):
-        break
-    brString = sizeFinder.getText().encode('utf-8')
-    urlIndex+=1
-
 # TODO: Check to see if highest resolution is zero or best index is -1
 
-imagePage = IMAGE_SEARCH + (urls[bestIndex])[DELETION_SIZE:]
+for imageType in ACCEPTABLE_IMAGE_FORMATS:
+    if(imageType in urls[bestIndex]):
+        imagePage = (urls[bestIndex])[IMAGE_SEARCH:(string.find(urls[bestIndex]
+                     , imageType) + len(imageType))]
 
 print(imagePage)
-"""
-# Get rid of the 
-# '/url?q=' and replace it with 'https://www.google.com/imgres?imgurl='
-# this will take you to another page. Grab the source html again and put it
-# in the soup to read again.
-# look for '<a target="_blank"...' and grab the url from after 'data-href='
-# This bring you to the image. Download it
